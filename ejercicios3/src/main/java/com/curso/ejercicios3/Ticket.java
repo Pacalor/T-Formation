85d6c763ae1695ea98e37d9f497ef875a9f6a6e0
Padilla Montero <dpadilla@t-systems.es>
2022-10-11 13:42:38 +0200

    Exercise init

---
+++ ejercicios3/src/main/java/com/curso/ejercicios3/Ticket.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.ejercicios3;
+
+import static com.curso.ejercicios3.Constants.*;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author dpadilla
+ */
+public class Ticket {
+    private int price;
+    private static int totalId=0;
+    private int id;
+    private GregorianCalendar date;
+
+    public Ticket(GregorianCalendar date) {
+        this.id = totalId++;
+        this.price = PRICE;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+    
+    
+    
+    
+}
